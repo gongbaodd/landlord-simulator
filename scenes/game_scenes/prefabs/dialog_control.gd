e678bf1e22f81f9ed5be4e716647486807acebf9
gongbaodd@ProArt <gongbaodd@qq.com>
2025-06-25 22:05:18 +0300

    some logic is different in release build

--- scenes/game_scenes/prefabs/dialog_control.gd
+++ scenes/game_scenes/prefabs/dialog_control.gd
@@ -3,10 +3,15 @@ extends Control
 
 @onready var dialogue_box = $DialogueBox
 
+func _ready():
+	dialogue_box.visible = false
+
 func show_dialogue(dialogData: DialogueData) -> void:
+	dialogue_box.visible = true
 	if not dialogue_box.is_running():
 		dialogue_box.data = dialogData
 		dialogue_box.start()
 
 func hide_dialogue() -> void:
 	dialogue_box.stop()
+	dialogue_box.visible = false
