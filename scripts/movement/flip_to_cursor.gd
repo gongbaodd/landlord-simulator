8dc4c3d6ff53f210fc9b91a1dd0c7f6e23a710ae
gongbaodd@ProArt <gongbaodd@qq.com>
2025-06-26 18:00:56 +0300

    fix player movement

--- scripts/movement/flip_to_cursor.gd
+++ scripts/movement/flip_to_cursor.gd
@@ -1,9 +1,1 @@
-class_name FlipToCursor extends Node2D
-
-func _process(_delta):
-	var mouse_pos = get_global_mouse_position()
-	var angle = abs(global_position.angle_to_point(mouse_pos))
-	if angle >= PI / 2:
-		scale.x = -1 * abs(scale.x)
-	elif angle < PI / 2:
-		scale.x = abs(scale.x)
+extends Node2D
--- scripts/movement/top_down_controller.gd
+++ scripts/movement/top_down_controller.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-var speed = 500
+@export var dialog_control: DialogControl
+const speed = 500
 
 func _physics_process(_delta: float) -> void:
-	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * speed
-	move_and_slide()
+	if !dialog_control.dialogue_box.visible:
+		var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+		velocity = direction * speed
+		move_and_slide()
