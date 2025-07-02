c8fec9ed00de9963d7877657f4d6713654cf41ed
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-02 18:57:36 +0300

    Too many need to change, only door dialogue done

--- scenes/game_scenes/prefabs/house.gd
+++ scenes/game_scenes/prefabs/house.gd
@@ -1,17 +1,10 @@
 extends Area2D
 
-# @export var dialog_data: DialogueData
-@export var money_label: Label
-
-const house_price = 1500
-
 var player_inside := false
-
 
 func _process(_delta: float) -> void:
 	if player_inside and Input.is_action_just_pressed("use"):
-		pass
-		# dialog_control.show_dialogue(dialog_data,)
+		DialogueManager.show_example_dialogue_balloon(load("res://scenes/game_scenes/dialogues/house.dialogue"), "start")
 
 
 func _on_body_entered(body:Node2D) -> void:
@@ -21,13 +14,3 @@ func _on_body_entered(body:Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		player_inside = false
-
-signal on_buy_house_succeed
-signal on_buy_house_failed
-
-func _on_dialog_on_buy_house() -> void:
-	if (money_label.money_count - house_price) > 0:
-		money_label.money_count -= house_price
-		on_buy_house_succeed.emit()
-	else:
-		on_buy_house_failed.emit()
--- scenes/game_scenes/preloads/game_manager.gd
+++ scenes/game_scenes/preloads/game_manager.gd
@@ -2,10 +2,20 @@ extends Node2D
 
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var time: Label = canvas_layer.get_node("Time")
+@onready var money_label: Label = canvas_layer.get_node("Money")
 
 func _ready() -> void:
-    canvas_layer.visible = false
+	canvas_layer.visible = false
 
 func start_game():
-    canvas_layer.visible = true
-    time.start_countdown()
+	canvas_layer.visible = true
+	time.start_countdown()
+
+const house_price = 1500
+
+func buy_house() -> bool:
+	if (money_label.money_count - house_price) > 0:
+		money_label.money_count -= house_price
+		return true
+	else:
+		return false
