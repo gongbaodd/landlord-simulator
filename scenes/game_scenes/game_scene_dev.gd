6593d29d44322e41b82d73d07fb00906c1d6fac5
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-02 18:19:36 +0300

    add dialogue manager plugin

--- scenes/game_scenes/game_scene_dev.gd
+++ scenes/game_scenes/game_scene_dev.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 func _ready() -> void:
     GameManager.start_game()
-    
+    DialogueManager.show_example_dialogue_balloon(
+        load("res://scenes/game_scenes/dialogues/test.dialogue"), 
+        "start"
+    )
--- scenes/game_scenes/prefabs/player.gd
+++ scenes/game_scenes/prefabs/player.gd
@@ -4,7 +4,7 @@ const speed = 500
 @onready var animation_tree: AnimationTree = get_node("animation").get_node("AnimationTree")
 
 func _physics_process(_delta: float) -> void:
-	if !GameManager.dialog_control.dialogue_box.visible:
+	# if !GameManager.dialog_control.dialogue_box.visible:
 		var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 		velocity = direction * speed
 		animation_tree.set("parameters/blend_position", direction)
