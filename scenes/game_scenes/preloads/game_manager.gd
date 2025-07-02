5b72ec982d0b248e6017dc87d6da21779abd89a6
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-02 14:05:29 +0300

    add start_game in game_manager

--- scenes/game_scenes/preloads/game_manager.gd
+++ scenes/game_scenes/preloads/game_manager.gd
@@ -1,3 +1,11 @@
 extends Node2D
 
 @export var dialog_control: DialogControl
+
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
+func _ready() -> void:
+    canvas_layer.visible = false
+
+func start_game():
+    canvas_layer.visible = true
