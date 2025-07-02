f0893e04e21d32aa9a6b7d57c9c463153f37bd12
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-02 18:16:42 +0300

    add Dialogue Manager

--- scripts/ui/viewport_container.gd
+++ scripts/ui/viewport_container.gd
@@ -3,7 +3,13 @@
 extends AspectRatioContainer
 
 func _ready() -> void:
+	DialogueManager.show_example_dialogue_balloon(
+        load("res://scenes/game_scenes/dialogues/test.dialogue"), 
+        "start"
+    )
 	var viewport_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	var viewport_height = ProjectSettings.get_setting("display/window/size/viewport_height")
 	
 	size = Vector2(viewport_width, viewport_height)
+
+
