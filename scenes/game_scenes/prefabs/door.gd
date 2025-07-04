4233ac57cf44f98291d204f4b849f9519145b1de
gongbaodd@ProArt <gongbaodd@qq.com>
2025-07-04 17:14:10 +0300

    check if has key

--- scenes/game_scenes/prefabs/door.gd
+++ scenes/game_scenes/prefabs/door.gd
@@ -4,17 +4,15 @@ extends Area2D
 @export var transition: PackedScene
 
 var player_inside := false
-var has_key := true
 var open_door_dialogue := load("res://scenes/game_scenes/dialogues/interact.dialogue")
 var world_scene := load("res://scenes/game_scenes/game_scene_dev.tscn")
 
 func _ready() -> void:
-	if not room_scene: room_scene = load("res://scenes/game_scenes/game_scene_dev.tscn") # will cause circular dependency if used in the export
-	has_key = GameManager.door_states[name]["has_key"]
+	if not room_scene: room_scene = world_scene # will cause circular dependency if used in the export
 
 func _process(_delta: float) -> void:
 	if player_inside and Input.is_action_just_pressed("use"):
-		if has_key:
+		if _check_if_has_key():
 			GameManager.running_dialog = null
 			SceneTransitionManager.change_scene_with_transition(
 				room_scene,
@@ -24,10 +22,15 @@ func _process(_delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		player_inside = true
-		if has_key:
+		if _check_if_has_key():
 			GameManager.running_dialog = DialogueManager.show_example_dialogue_balloon(open_door_dialogue, "start")
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		player_inside = false
+
+func _check_if_has_key() -> bool:
+	if GameManager.door_states.has(name):
+		return GameManager.door_states[name]["has_key"]
+	return true #indoor
 
